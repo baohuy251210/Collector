e627e1cec8fe5b05ffd76b3aed36beb7ce42db5c
Huy Tran <baohuy251210@gmail.com>
2020-08-26 20:41:32 -0600

    scheduled scraper

--- scraper.bat
+++ scraper.bat
@@ -6,6 +6,7 @@ C:\Users\baohu\anaconda3\envs\collector\python.exe "C:\Users\baohu\Desktop\Fall2
 echo.
 echo Making git changes...
 echo.
+git status
 git add -A
 git commit -m "scheduled scraper"
 git push 
