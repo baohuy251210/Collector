f7cdca709e333029028b422549a5f9a6b074e29f
Huy Tran <baohuy251210@gmail.com>
2020-08-26 19:12:05 -0600

    Basic scrape implemented

--- collectuofu.py
+++ collectuofu.py
@@ -46,7 +46,7 @@ def retrieve_covidpanel():
 
 
 def create_initcsv():
-    """one time use: init a csv for data storing 
+    """one time use: init a csv for data storing
     data/uofucovidinit_timeline.csv
     data/uofucovid_timeline.csv
     """
@@ -55,22 +55,48 @@ def create_initcsv():
     time = datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M')
     csv_name = './data/uofucovidinit_timeline.csv'
     with open(csv_name, 'w', encoding='utf-8') as csv_file:
-        field_names = ['time', 'retrieved']
+        field_names = ['time-MDT', 'retrieved']
         writer = csv.writer(csv_file)
         writer.writerow(field_names)
         writer.writerow([str(time), init])
     print('NEW INIT Created, this will recreate csv files')
 
 
+def create_datacsv():
+    """
+    since retriever function return a list
+    We'll derive only the number of self-reported case.
+    """
+    '''
+    We only have data about self-reported case for now:
+    see uofucovidinit_timeline.csv for full details on scraping
+    '''
+
+    csv_name = './data/uofucovid_timeline.csv'
+    selfreport_cases = int(retrieve_covidpanel()[3].split()[0])
+    time = datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M')
+
+    with open(csv_name, 'w', encoding='utf-8') as csv_file:
+        field_names = ['time-MDT', 'self-reported cases']
+        writer = csv.writer(csv_file)
+        writer.writerow(field_names)
+        writer.writerow([str(time), selfreport_cases])
+
+    print('uofucovid_timeline.csv created')
+
+
+# create_datacsv()
 # print(datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M'))
 # create_initcsv()
 # print(retrieve_covidpanel())
+
 
 def updater_initcsv():
     """
     This updates the csv if the covid panel is different.
     Could be useful if the html changes the order. (to redo the retriever)
     """
+    print('---covid panel scrape---')
     init_new = '|'.join(retrieve_covidpanel())
     time = datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M')
     init_old = ''
@@ -81,13 +107,48 @@ def updater_initcsv():
         init_old = lastrow['retrieved']
 
     if (init_old != init_new):
-        print('Detected new data')
+        print('(full)Detected new data')
         with open('./data/uofucovidinit_timeline.csv', 'a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow([time, init_new])
-            print('New data updated')
+            print('(full)New data updated')
     else:
-        print('No new update detected')
+        print('(full)No new update detected')
 
 
-updater_initcsv()
+# updater_initcsv()
+
+def updater_datacsv():
+    """
+    This function updates the cases count csv file
+    """
+    print('---cases scrape---')
+    csv_name = './data/uofucovid_timeline.csv'
+    selfreport_cases_new = int(retrieve_covidpanel()[3].split()[0])
+    time = datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M')
+
+    # compare with last row
+    with open('./data/uofucovid_timeline.csv', 'r') as csv_readfile:
+        reader = csv.DictReader(csv_readfile)
+        lastrow = list(reader)[-1]
+        selfreport_cases_old = lastrow['self-reported cases']
+
+    if (int(selfreport_cases_old) != int(selfreport_cases_new)):
+        print('(cases)Detected new data')
+        with open('./data/uofucovid_timeline.csv', 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([time, selfreport_cases_new])
+            print('(cases)New data updated')
+    else:
+        print('(cases)No new update detected')
+
+
+def main():
+    print("-------Scraping COVID-19 Data from UOFU---------- ")
+    print(datetime.now(timezone('America/Denver')).strftime('%Y-%m-%d %H:%M:%S'))
+    updater_initcsv()
+    updater_datacsv()
+
+
+if __name__ == "__main__":
+    main()
